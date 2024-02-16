599a942a48004be6779b38d8be3ba80b4f7aa22f
Nikita Starostin <n.starostin@itransition.com>
2024-02-16 03:41:23 +0300

    remove not working binding

--- lua/shinshil/remap.lua
+++ lua/shinshil/remap.lua
@@ -3,5 +3,4 @@ vim.g.cursorui = ""
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<C-l>", vim.cmd.Lazy)
-vim.keymap.set("i", "<C-space>", "<Cmd>lua vim.lsp.buf.hover()<CR>")
 
