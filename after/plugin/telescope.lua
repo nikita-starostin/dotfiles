640012eefc004d3645ca96a6a187ffe51ab5ad09
Nikita Starostin <n.starostin@itransition.com>
2024-02-17 19:23:02 +0300

    allow search in hideen directories, ignore .git; use rg, which is fastest and ignores items in gitignore

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,19 +1,40 @@
-local actions = require('telescope.actions')
-require('telescope').setup {
+local telescopeActions = require('telescope.actions')
+local telescope = require('telescope')
+local telescopeConfig = require("telescope.config")
+
+-- Clone the default Telescope configuration
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+-- I want to search in hidden/dot files.
+table.insert(vimgrep_arguments, "--hidden")
+-- I don't want to search in the `.git` directory.
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
+telescope.setup {
 	defaults = {
+		vimgrep_arguments = vimgrep_arguments,
 		mappings = {
 			i = {
+				["<esc>"] = telescopeActions.close,
+				["<C-u>"] = false,
 				["<C-j>"] = {
-					actions.move_selection_next, type = "action",
+					telescopeActions.move_selection_next, type = "action",
 					opts = { nowait = true, silent = true }
 				},
 				["<C-k>"] = {
-					actions.move_selection_previous, type = "action",
+					telescopeActions.move_selection_previous, type = "action",
 					opts = { nowait = true, silent = true }
 				},
 			}
 		}
-	}
+	},
+	pickers = {
+		find_files = {
+			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+			find_command = { "rg", "--files", "--hidden", "--glob", "!.git" },
+		},
+	},
 }
 
 local builtin = require('telescope.builtin')
