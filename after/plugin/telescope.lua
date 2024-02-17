54791a0005903629af3b15b5a781f865efdfd163
Nikita Starostin <n.starostin@itransition.com>
2024-02-17 04:00:31 +0300

    apply formatter

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,16 +1,16 @@
 local actions = require('telescope.actions')
 require('telescope').setup {
 	defaults = {
-		 mappings = {
-				i = {
-					["<C-j>"] = {
-						actions.move_selection_next, type = "action",
-						opts = { nowait = true, silent = true }
-					},
-					["<C-k>"] = {
-						actions.move_selection_previous, type = "action",
-						opts = { nowait = true, silent = true }
-					},
+		mappings = {
+			i = {
+				["<C-j>"] = {
+					actions.move_selection_next, type = "action",
+					opts = { nowait = true, silent = true }
+				},
+				["<C-k>"] = {
+					actions.move_selection_previous, type = "action",
+					opts = { nowait = true, silent = true }
+				},
 			}
 		}
 	}
@@ -20,7 +20,7 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-		builtin.grep_string({ search = vim.fn.input("Grep > ") });
+	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 
 vim.keymap.set('n', '<C-a>', builtin.commands, {})
