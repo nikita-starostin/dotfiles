b0d77a3996d1bb122cf490232ccadadae7a7fb7a
Nikita Starostin <n.starostin@itransition.com>
2024-02-17 20:37:55 +0300

    remap telescope to less keystrokes

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -42,9 +42,9 @@ local utils = require('telescope.utils')
 vim.keymap.set('n', '<leader>ff', function()
 	builtin.find_files({ cwd = utils.buffer_dir() })
 end)
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>p', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set('n', '<leader>s', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 
