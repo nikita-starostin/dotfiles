7aecd42702f3b9f0f2b88dad3dad1744eefa3e74
Nikita Starostin <n.starostin@itransition.com>
2024-02-19 14:27:10 +0300

    use paper color as default theme

--- lua/shinshil/plugins/theme.lua
+++ lua/shinshil/plugins/theme.lua
@@ -7,7 +7,7 @@
 -- cattpuccin installed
 -- blue, lightblue - two theme close to turbo pascal
 function SetMyTheme(color)
-	color = color or "slate"
+	color = color or "PaperColor"
 	vim.cmd.colorscheme(color)
 	-- make the nvim itself 0 opacity, the terminal would be used as background
 	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
