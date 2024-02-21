b45831b0562a198636a74da84fa799d00655722b
Nikita Starostin <n.starostin@itransition.com>
2024-02-22 02:16:29 +0300

    Add harpoon

--- lua/shinshil/plugins/autosave.lua
+++ lua/shinshil/plugins/autosave.lua
@@ -1,3 +1,19 @@
 return {
-	"pocco81/auto-save.nvim"
+	"okuuva/auto-save.nvim",
+	config = function()
+		require("auto-save").setup({
+			enabled = true,
+			condition = function(buf)
+				local fn = vim.fn
+
+				-- that is to sync with Harpoon menu
+				if fn.getbufvar(buf, "&filetype") == 'harpoon'
+				then
+					return false
+				end
+				return true
+			end
+		});
+		vim.api.nvim_set_keymap("n", "<leader>ta", ":ASToggle<CR>", {});
+	end
 }
