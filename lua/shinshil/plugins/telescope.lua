57536072f6c47d9f770ff92d4036e3d6d414f0d7
Nikita Starostin <n.starostin@itransition.com>
2024-02-23 00:37:05 +0300

    Add ability to live grep selection

--- lua/shinshil/plugins/telescope.lua
+++ lua/shinshil/plugins/telescope.lua
@@ -1,3 +1,16 @@
+function vim.getVisualSelection()
+  vim.cmd('noau normal! "vy"')
+  local text = vim.fn.getreg('v')
+  vim.fn.setreg('v', {})
+
+  text = string.gsub(text, "\n", "")
+  if #text > 0 then
+    return text
+  else
+    return ''
+  end
+end
+
 return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.5',
@@ -68,9 +81,14 @@ return {
     end)
     vim.keymap.set('n', '<leader>w', builtin.find_files, {})
     vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-    vim.keymap.set('n', '<leader>s', function()
+    vim.keymap.set('n', '<leader>sg', function()
       builtin.grep_string({ search = vim.fn.input("Grep > ") });
     end)
+    vim.keymap.set('v', '<leader>s', function()
+      local text = vim.getVisualSelection()
+      builtin.live_grep({ default_text = text })
+    end)
+    vim.keymap.set('n', '<leader>sl', builtin.live_grep)
 
     vim.keymap.set('n', '<C-a>', builtin.commands, {})
     vim.keymap.set('n', '<C-q>', builtin.builtin, {})
