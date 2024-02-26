3b0d6e61bc738448b3fe069ca8cfbc5362dae858
Nikita Starostin <n.starostin@itransition.com>
2024-02-27 00:01:27 +0300

    simplify lualine

--- lua/shinshil/plugins/lualine.lua
+++ lua/shinshil/plugins/lualine.lua
@@ -35,12 +35,12 @@ return {
           }
         },
         sections = {
-          lualine_a = { { 'filename', path = 1 } },
+          lualine_a = { },
           lualine_b = { 'branch' },
           lualine_c = { 'diff', 'diagnostics' },
-          lualine_x = { CTimeLine },
-          lualine_y = { 'location' },
-          lualine_z = {}
+          lualine_x = { },
+          lualine_y = { },
+          lualine_z = { CTimeLine }
         },
         inactive_sections = {
           lualine_a = {},
