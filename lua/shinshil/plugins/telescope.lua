9c98350beda87473c518dcfbf03c6594b31cb0da
Nikita Starostin <n.starostin@itransition.com>
2024-11-09 18:57:40 +0300

    extract telescope setup to separate file

--- lua/shinshil/plugins/telescope.lua
+++ lua/shinshil/plugins/telescope.lua
@@ -20,66 +20,7 @@ return {
     tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-      local function filename_first(_, path)
-        local tail = vim.fs.basename(path)
-        local parent = vim.fs.dirname(path)
-        if parent == "." then
-          return tail
-        end
-        return string.format("%s\t\t%s", tail, parent)
-      end
-
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = "TelescopeResults",
-        callback = function(ctx)
-          vim.api.nvim_buf_call(ctx.buf, function()
-            vim.fn.matchadd("TelescopeParent", "\t\t.*$")
-            -- vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
-          end)
-        end,
-      })
-
-      -- Clone the default Telescope configuration and allow search for hidden, but exclude .git
-      local telescopeConfig = require("telescope.config")
-      local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
-      table.insert(vimgrep_arguments, "--hidden")
-      table.insert(vimgrep_arguments, "--glob")
-      table.insert(vimgrep_arguments, "!**/.git/*")
-
-      local telescopeActions = require('telescope.actions')
-      local telescope = require('telescope')
-      telescope.setup {
-        defaults = {
-          wrap_results = true,
-          path_display = filename_first,
-          vimgrep_arguments = vimgrep_arguments,
-          file_ignore_patterns = {
-            "node_modules",
-            ".next",
-          },
-          mappings = {
-            i = {
-              ["<C-s>"] = telescopeActions.file_vsplit,
-              ["<esc>"] = telescopeActions.close,
-              ["<C-u>"] = false,
-              ["<C-j>"] = {
-                telescopeActions.move_selection_next, type = "action",
-                opts = { nowait = true, silent = true }
-              },
-              ["<C-k>"] = {
-                telescopeActions.move_selection_previous, type = "action",
-                opts = { nowait = true, silent = true }
-              },
-            }
-          }
-        },
-        pickers = {
-          find_files = {
-            -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-            find_command = { "rg", "--files", "--hidden", "--glob", "!.git" },
-          },
-        },
-      }
+      require('shinshil.setup.telescope_setup');
     end
   }
 }
--- lua/shinshil/remap/custom_remap.lua
+++ lua/shinshil/remap/custom_remap.lua
@@ -4,7 +4,6 @@ vim.keymap.set("n", "<leader>eb",
 
 -- set tab size to 2
 vim.keymap.set("n", "<leader>2", ":lua SetTabSize(2)<CR>", { desc = "Set tab size to 2" })
--- set tab size to 4
 vim.keymap.set("n", "<leader>4", ":lua SetTabSize(4)<CR>", { desc = "Set tab size to 4" })
 
 -- toggle dark and light theme
@@ -17,3 +16,33 @@ vim.keymap.set("n", "<leader>tt", function()
   end
 end, { desc = "Toggle dark and light theme" })
 
+-- show info about current file
+function ShowInfo()
+  vim.api.nvim_command("echo expand('%:p') . ' | ' . line('.') . ':' . col('.')")
+end
+vim.keymap.set("n", "<leader>h", ShowInfo, { nowait = false, desc = "Show current file info" })
+
+-- show current status of the current branch
+function ShowBranch()
+  local resultMessage = ""
+  local branch = vim.fn.system("git branch --show-current")
+  resultMessage = resultMessage .. branch
+  local remoteBranchName = vim.fn.system("git rev-parse --abbrev-ref --symbolic-full-name @{u}")
+  resultMessage = resultMessage .. remoteBranchName
+  local isEverythingCommitted = vim.fn.system("git status --porcelain")
+  resultMessage = resultMessage .. "\r"
+  if isEverythingCommitted == "" then
+    resultMessage = resultMessage .. "committed"
+  else
+    resultMessage = resultMessage .. "uncommited"
+  end
+  resultMessage = resultMessage .. " | "
+  local isEverythingPushed = vim.fn.system("git status --porcelain")
+  if isEverythingPushed == "" then
+    resultMessage = resultMessage .. "pushed"
+  else
+    resultMessage = resultMessage .. "unpushed"
+  end
+  vim.api.nvim_command("echo '" .. resultMessage .. "'")
+end
+vim.keymap.set("n", "<leader>b", ShowBranch, { nowait = false, silent = true })
