6a10fe391817b3fe0ea4a55626e98c89bb69b5a1
Nikita Starostin <n.starostin@itransition.com>
2024-11-12 14:05:46 +0300

    change scratch remap configuration

--- lua/shinshil/remap/scratch_remap.lua
+++ lua/shinshil/remap/scratch_remap.lua
@@ -1,2 +1,2 @@
-vim.keymap.set('n', '<leader>os', ':ScratchOpen<CR>', { desc = "Open scratch" })
-vim.keymap.set('n', '<leader>cs', ':ScratchWithName<CR>', { desc = "Create scratch" })
+vim.keymap.set('n', '<leader>os', ':ScratchOpen<CR>', { desc = "Open scratch file" })
+vim.keymap.set('n', '<leader>as', ':ScratchWithName<CR>', { desc = "Add scratch file" })
