171a355f1b7f259efa910dba8e943481e62f0be0
Nikita Starostin <n.starostin@itransition.com>
2024-11-12 14:47:48 +0300

    add edit utils

---
+++ lua/shinshil/plugins/edit-utils.lua
@@ -0,0 +1,19 @@
+return {
+  -- plugin to repeat some actions with ., e.g. vim-surround
+  {
+    'tpope/vim-repeat',
+  },
+
+  -- plugin to comment/uncomment code
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+      -- add any options here
+    }
+  },
+
+  -- plugin to surround text with brackets, quotes, etc.
+  {
+    'tpope/vim-surround',
+  },
+}
