ed346888f2d25ef35c6880225df7664fb4149c6f
Nikita Starostin <n.starostin@itransition.com>
2025-02-13 06:11:40 +0300

    tmp disable copilot

--- lua/shinshil/plugins/ai.lua
+++ lua/shinshil/plugins/ai.lua
@@ -1,24 +1,24 @@
 return {
-  {
-    'github/copilot.vim',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      vim.g.copilot_no_tab_map = true;
-      vim.g.copilot_assume_mapped = true;
-      vim.g.copilot_tab_fallback = "";
-      -- The mapping is set to other key, see custom/lua/mappings
-      -- or run <leader>ch to see copilot mapping section
-      vim.keymap.set('i', '<C-l>',
-        function()
-          vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
-        end,
-        {
-          expr = true,
-          replace_keycodes = true,
-          nowait = true,
-          silent = true,
-          noremap = true
-        })
-    end
-  }
+  -- {
+  --   'github/copilot.vim',
+  --   event = { 'BufReadPre', 'BufNewFile' },
+  --   config = function()
+  --     vim.g.copilot_no_tab_map = true;
+  --     vim.g.copilot_assume_mapped = true;
+  --     vim.g.copilot_tab_fallback = "";
+  --     -- The mapping is set to other key, see custom/lua/mappings
+  --     -- or run <leader>ch to see copilot mapping section
+  --     vim.keymap.set('i', '<C-l>',
+  --       function()
+  --         vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+  --       end,
+  --       {
+  --         expr = true,
+  --         replace_keycodes = true,
+  --         nowait = true,
+  --         silent = true,
+  --         noremap = true
+  --       })
+  --   end
+  -- }
 }
