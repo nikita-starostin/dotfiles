63d36aa27cf4a9dbbc1a63011c7ea97b49da5423
Nikita Starostin <n.starostin@itransition.com>
2025-09-24 09:30:56 +0100

    setup omnisharp

--- lua/shinshil/plugins/lsp.lua
+++ lua/shinshil/plugins/lsp.lua
@@ -91,7 +91,7 @@ return {
           { name = 'nvim_lsp' },
           { name = 'buffer' },
           { name = 'orgmode' }
-        },
+       },
       }
 
       require('mason').setup({})
@@ -102,6 +102,49 @@ return {
           lua_ls = function()
             local lua_opts = lsp_zero.nvim_lua_ls()
             local lspConfig = require('lspconfig')
+
+            lspConfig.omnisharp.setup {
+                cmd = { "dotnet", "C:/Users/n.starostin/AppData/Local/nvim-data/mason/packages/omnisharp/libexec/OmniSharp.dll" },
+                settings = {
+                  FormattingOptions = {
+                    -- Enables support for reading code style, naming convention and analyzer
+                    -- settings from .editorconfig.
+                    EnableEditorConfigSupport = true,
+                    -- Specifies whether 'using' directives should be grouped and sorted during
+                    -- document formatting.
+                    OrganizeImports = nil,
+                  },
+                  MsBuild = {
+                    -- If true, MSBuild project system will only load projects for files that
+                    -- were opened in the editor. This setting is useful for big C# codebases
+                    -- and allows for faster initialization of code navigation features only
+                    -- for projects that are relevant to code that is being edited. With this
+                    -- setting enabled OmniSharp may load fewer projects and may thus display
+                    -- incomplete reference lists for symbols.
+                    LoadProjectsOnDemand = nil,
+                  },
+                  RoslynExtensionsOptions = {
+                    -- Enables support for roslyn analyzers, code fixes and rulesets.
+                    EnableAnalyzersSupport = true,
+                    -- Enables support for showing unimported types and unimported extension
+                    -- methods in completion lists. When committed, the appropriate using
+                    -- directive will be added at the top of the current file. This option can
+                    -- have a negative impact on initial completion responsiveness,
+                    -- particularly for the first few completion sessions after opening a
+                    -- solution.
+                    EnableImportCompletion = true,
+                    -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+                    -- true
+                    AnalyzeOpenDocumentsOnly = nil,
+                  },
+                  Sdk = {
+                    -- Specifies whether to include preview versions of the .NET SDK when
+                    -- determining which version to use for project loading.
+                    IncludePrereleases = true,
+                  },
+                },
+            }
+
             lspConfig.lua_ls.setup(lua_opts)
             lspConfig.tailwindcss.setup({
               settings = {
@@ -110,7 +153,7 @@ return {
                     classRegex = {
                       "cva\\(([^)]*)\\)",                                      -- Basic cva() pattern
                       "cva\\([\\s\\t]*['\"]([^'\"]*)['\"]",                    -- First string arg
-                      "['\"`]([^'\"`]*).*?['\"`]\\s*[),]",                     -- Any string in arguments
+                      -- "['\"`]([^'\"`]*).*?['\"`]\\s*[),]",                     -- Any string in arguments
                       "variants:\\s*{[^}]*\\w+:\\s*['\"]([^'\"]*)['\"]",       -- Variant values
                       "defaultVariants:\\s*{[^}]*\\w+:\\s*['\"]([^'\"]*)['\"]" -- Default variants
                     }
